a434867024436500bb2d3ed85155f30daf3b9ba4
NDAYISENGAManu <webmasterio2015@gmail.com>
2022-08-04 12:42:12 +0200

    shopping cart working collectly

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
+
 const url = 'https://course-api.com/react-useReducer-cart-project'
+
 const AppContext = React.createContext()
 
 const initialState = {
@@ -12,12 +14,55 @@ const initialState = {
 }
 
 const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState) 
+
+  const clearCart = () => {
+    dispatch({type: 'CLEAR_CART'})
+  }
+  const remove = (id) => {
+    dispatch({type: 'REMOVE', payload: id})
+  }
+
+  const increase = (id) => {
+    dispatch({type: 'INCREASE', payload: id})
+  }
+
+  const decrease = (id) => {
+    dispatch({type: 'DECREASE', payload: id})
+  }
+
+  // let fetch data first, before this data api, we were having 3 cart 
+  //items from data.js but we are gooing to have a new api with 4 items
+
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING'})
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
+  useEffect(()=>{
+    dispatch({ type: 'GET_TOTALS' })
+  },[state.cart])
+
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TONGLE_AMOUNT', payload: { id, type }})
+  }
+
 
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        toggleAmount
       }}
     >
       {children}
