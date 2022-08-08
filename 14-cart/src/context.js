6b70fd85db60f49d8c8ad890e513daf6ed150750
NDAYISENGAManu <webmasterio2015@gmail.com>
2022-08-08 12:16:43 +0200

    menu with dynamic cactegories

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -1,23 +1,23 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
-
-const url = 'https://course-api.com/react-useReducer-cart-project'
-
-const AppContext = React.createContext()
-
-const initialState = {
-  loading: false,
-  cart: cartItems,
-  total: 0,
-  amount: 0,
-}
-
-const AppProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState) 
-
-  const clearCart = () => {
-    dispatch({type: 'CLEAR_CART'})
+import React, { useState, useContext, useReducer, useEffect } from 'react' 
+import cartItems from './data' 
+import reducer from './reducer' 
+ 
+const url = 'https://course-api.com/react-useReducer-cart-project' 
+  
+const AppContext = React.createContext() 
+ 
+const initialState = { 
+  loading: false, 
+  cart: cartItems, 
+  total: 0, 
+  amount: 0, 
+} 
+ 
+const AppProvider = ({ children }) => { 
+  const [state, dispatch] = useReducer(reducer, initialState)  
+ 
+  const clearCart = () => { 
+    dispatch({type: 'CLEAR_CART'}) 
   }
   const remove = (id) => {
     dispatch({type: 'REMOVE', payload: id})
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # react-projects
+
+# I am Emmanuel NDAYISENGA, a software developer specialized in front-end development & UI + Figma
+
+# JavaScript & TypeScript are my main language
+
+# I am also interested in DevOps
